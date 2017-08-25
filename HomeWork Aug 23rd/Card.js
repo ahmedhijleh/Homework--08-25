cf2bd83a9e9aa152cd13c3aff54d81f596fbe88d
Ahmed Abu-Hijleh <ahmed.abuhijleh90@gmail.com>
2017-08-25 09:53:06 -0700

    Add files via upload

---
+++ HomeWork Aug 23rd/Card.js
@@ -0,0 +1,13 @@
+
+
+// Creates a question,answer for each card
+var card = function(front,back) {
+  var  q1= new card("who was the first president of the United States?","George Washington");
+
+      if (err) {
+        console.log(err);
+      }
+      console.log(JSON.stringify(result, null, 2));
+   
+
+module.exports = card;
